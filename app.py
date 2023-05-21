4ad1c067e6e4987235fd6fbf15aeeb62e4082978
N0N4M3D-b <20wlsaud@gmail.com>
2023-05-21 21:38:06 +0900

    Add Skeleton code

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from flask import Flask
-from flask_restx import Api, Resource
+from flask_restx import Api
+from flask_restx import Resource
+import pymysql
+
+def connect_database():
+    db = pymysql.connect(host='database',
+        port=3306,
+        user='finaluser',
+        passwd='dongafinal1234!',
+        db='final',
+        charset='utf8'
+    )
+    cursor = db.cursor()
+
+    return db, cursor
+
+def disconnect_database(db):
+    db.commit()
+    db.close()
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,7 +25,38 @@ api = Api(app)
 @api.route('/hello')
 class HelloWorld(Resource):
     def get(self):
-        return {'message': 'Hello, Worajdold!'}
+        return {'message': 'Hello, World!'}
+
+@api.route('/data')
+class Data(Resource):
+    def post(self):
+        pass
+
+@api.route('/data/solved')
+class Solved(Resource):
+    def get(self):
+        pass
+
+@api.route('/data/solved/<int:id>')
+class SolvedID(Resource):
+    def get(self):
+        pass
+
+    def put(self):
+        pass
+
+@api.route('/data/unsolved')
+class UnSolved(Resource):
+    def get(self):
+        pass
+
+@api.route('/data/unsolved/<int:id>')
+class UnSolvedID(Resource):
+    def get(self):
+        pass
+
+    def put(self):
+        pass
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8080', debug=True)
