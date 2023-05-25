8daae1c6b5f89ef058322aef6057e69e9a590914
N0N4M3D-b <20wlsaud@gmail.com>
2023-05-25 07:31:51 +0000

    Complete Basic implementation of Backend API

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ def connect_database():
     return db, cursor
 
 def disconnect_database(db):
-    db.commit()
     db.close()
 
 app = Flask(__name__)
@@ -38,6 +37,8 @@ class Data(Resource):
         db, cursor = connect_database()
         query = 'INSERT INTO UnsolvedCase (latitude, longitude, image) VALUES(%s,%s,%s)'
         cursor.execute(query, (lat, lon, pic))
+        db.commit()
+
         disconnect_database(db)
 
         return {'message': 'Add data success', 'status': 200}
@@ -45,28 +46,145 @@ class Data(Resource):
 @api.route('/data/solved')
 class Solved(Resource):
     def get(self):
-        pass
+        db, cursor = connect_database()
+        query = 'SELECt * FROM SolvedCase'
+        cursor.execute(query)
+        result = cursor.fetchall()
+        disconnect_database(db)
 
-@api.route('/data/solved/<int:id>')
+        ret = {"datas" : []}
+        tmp_data = {}
+
+        for data in result:
+            tmp_data["data_id"] = data[0]
+            tmp_data["latitude"] = data[1]
+            tmp_data["longitude"] = data[2]
+            tmp_data["pic"] = data[3]
+
+            ret["datas"].append(tmp_data)
+
+        ret["message"] = "Get solved datas success"
+        ret["status"] = 200
+
+        return ret
+
+@api.route('/data/solved/<int:caseNum>')
 class SolvedID(Resource):
-    def get(self):
-        pass
+    def get(self, caseNum):
+        db, cursor = connect_database()
+        query = f'SELECT * FROM SolvedCase WHERE caseNum={caseNum}'
+        cursor.execute(query)
+        result = cursor.fetchall()
+        data = result[0]
+        disconnect_database()
 
-    def put(self):
-        pass
+        tmp_data = {}
+        tmp_data["caseNum"] = data[0]
+        tmp_data["latitude"] = data[1]
+        tmp_data["longitude"] = data[2]
+        tmp_data["pic"] = data[3]
+
+        ret = {"data" : tmp_data}
+
+        ret["message"] = "Get solved data success"
+        ret["status"] = 200
+
+        return ret
+
+    def put(self, caseNum):
+        db, cursor = connect_database()
+        query = f'SELECT * FROM SolvedCase WHERE caseNum={caseNum}'
+        cursor.execute(query)
+        result = cursor.fetchall()
+        data = result[0]
+        
+        query = 'INSERT INTO UnsolvedCase (latitude, longitude, image) VALUES(%s,%s,%s)'
+        cursor.execute(query, (data[1], data[2], data[3]))
+        db.commit()
+
+        query = f'DELETE FROM SolvedCase WHERE caseNum={data[0]}'
+        cursor.execute(query)
+        db.commit()
+
+        disconnect_database(db)
+
+        ret = {}
+        ret["message"] = "SolvedCase changed to UnsolvedCase"
+        ret["status"] = 200
+
+        return ret
 
 @api.route('/data/unsolved')
 class UnSolved(Resource):
     def get(self):
-        pass
+        db, cursor = connect_database()
+        query = 'SELECt * FROM UnsolvedCase'
+        cursor.execute(query)
+        result = cursor.fetchall()
+        disconnect_database(db)
 
-@api.route('/data/unsolved/<int:id>')
-class UnSolvedID(Resource):
-    def get(self):
-        pass
+        ret = {"datas" : []}
+        tmp_data = {}
 
-    def put(self):
-        pass
+        for data in result:
+            tmp_data["caseNum"] = data[0]
+            tmp_data["latitude"] = data[1]
+            tmp_data["longitude"] = data[2]
+            tmp_data["pic"] = data[3]
+
+            ret["datas"].append(tmp_data)
+
+
+        ret["message"] = "Get unsolved datas success"
+        ret["status"] = 200
+
+        return ret
+
+@api.route('/data/unsolved/<int:caseNum>')
+class SolvedID(Resource):
+    def get(self, caseNum):
+        db, cursor = connect_database()
+        query = f'SELECT * FROM UnsolvedCase WHERE caseNum={caseNum}'
+        cursor.execute(query)
+        result = cursor.fetchall()
+        data = result[0]
+        disconnect_database(db)
+
+        tmp_data = {}
+        tmp_data["caseNum"] = data[0]
+        tmp_data["latitude"] = data[1]
+        tmp_data["longitude"] = data[2]
+        tmp_data["pic"] = data[3]
+
+        ret = {"data" : tmp_data}
+
+        ret["message"] = "Get solved data success"
+        ret["status"] = 200
+
+        return ret
+
+    def put(self, caseNum):
+        db, cursor = connect_database()
+        query = f'SELECT * FROM UnsolvedCase WHERE caseNum={caseNum}'
+        cursor.execute(query)
+        result = cursor.fetchall()
+        data = result[0]
+        
+        query = 'INSERT INTO SolvedCase (latitude, longitude, image) VALUES(%s,%s,%s)'
+        cursor.execute(query, (data[1], data[2], data[3]))
+        db.commit()
+
+        query = f'DELETE FROM UnsolvedCase WHERE caseNum={data[0]}'
+        cursor.execute(query)
+        db.commit()
+
+        disconnect_database(db)
+
+        ret = {}
+        ret["message"] = "UnsolvedCase changed to SolvedCase"
+        ret["status"] = 200
+
+        return ret
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8080', debug=True)
--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-import base64
 import requests
+import hashlib
 
 with open('./test.png', 'rb') as f:
     img = f.read()
 
-img = base64.b64encode(img)
-img = img.decode('UTF-8')
+img_hash = hashlib.md5(img).hexdigest()
+print(img_hash)
 
-r = requests.post('http://localhost:8080/data', json={'latitude': '29.991393', 'longitude': '59.361260', 'pic': img})
-print(r.status_code)
+r = requests.post('http://localhost:8080/data', json={'latitude': '29.991393', 'longitude': '59.361260', 'pic': img_hash})
+#print(r.status_code)
