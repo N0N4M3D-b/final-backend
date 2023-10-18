13173d528999ba1e56e5d1fce5941b3aef1b28c4
N0N4M3D-b <20wlsaud@gmail.com>
2023-10-18 12:36:53 +0000

    Feat Register function

--- register.py
+++ register.py
@@ -2,6 +2,8 @@ from flask import request
 from flask_restx import Api
 from flask_restx import Resource
 from flask_restx import Namespace
+from initialize_database import connect_database
+from initialize_database import disconnect_database
 
 Register = Namespace('Register')
 
@@ -9,10 +11,10 @@ Register = Namespace('Register')
 class Signup(Resource):
     def post(self):
         try:
-            email = request.json.get('email')
-            name = request.json.get('name')
-            password = request.json.get('password')
-            tel = request.json.get('tel')
+            self.email = request.json.get('email')
+            self.name = request.json.get('name')
+            self.password = request.json.get('password')
+            self.tel = request.json.get('tel')
         except:
             print('[!] /signup (POST) : Invalid request data')
             return {
@@ -20,4 +22,41 @@ class Signup(Resource):
                 'status' : 400
             }
         
-        
+        # Check email WhiteList
+        # todo -> status 40000
+
+        # handle email exist in Profiles exception
+        if self.checkIsExist():
+            return {
+                'message' : 'Already exist email',
+                'status' : 40001
+            }
+    
+        self.insertDatabase()
+
+        return {
+            'message' : 'Signup success',
+            'status' : 200
+        }
+
+    def checkIsExist(self):
+        exist_flag = True
+
+        db, cursor = connect_database()
+
+        query = f'SELECT email FROM Profiles WHERE email = "{self.email}"'
+        cursor.execute(query)
+        if len(cursor.fetchall()) != 0:
+            print('[!] /signup (POST) : Exist email')
+            exist_flag = False
+
+        disconnect_database(db)
+
+        return exist_flag
+    
+    def insertDatabase(self):
+        db, cursor = connect_database()
+
+        qeury = f'INSERT INTO Profile VALUES ("{self.email}", "{self.name}", "{self.password}", "{self.tel}")'
+        cursor.execute(query)
+        db.commit()
