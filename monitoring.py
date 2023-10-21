4a7f646ae28a433ca73a7cea16136b5dfd42c433
N0N4M3D-b <20wlsaud@gmail.com>
2023-10-21 07:24:49 +0000

    Feature register unsolvedcase data

--- monitoring.py
+++ monitoring.py
@@ -2,6 +2,8 @@ from flask import request
 from flask_restx import Api
 from flask_restx import Resource
 from flask_restx import Namespace
+from initialize_database import connect_database
+from initialize_database import disconnect_database
 
 Monitoring = Namespace('Monitoring')
 
@@ -18,7 +20,35 @@ class SolvedGet(Resource):
 @Monitoring.route('/unsolved')
 class UnSolved(Resource):
     def post(self):
-        pass
+        try:
+            self.latitude = request.json.get('latitude')
+            self.longitude = request.json.get('longitude')
+            self.pic = request.json.get('pic')
+        except:
+            print('[!] /unsolved (POST) : Invalid request data')
+            return {
+                'message' : 'Invalid request data',
+                'status' : 400
+            }
+        
+        try:
+            self.UnsolvedInsertDatabase()
+        except:
+            return {
+                'message' : 'Database error',
+                'status' : 400
+            }
+        return {
+            'message' : 'Register unsolved case success',
+            'status' : 200
+        }
+        
+    def UnsolvedInsertDatabase(self):
+        db, cursor = connect_database()
+
+        query = f'INSERT INTO UnsolvedCase VALUES (NULL, "{self.latitude}", "{self.longitude}", "{self.pic}", CURRENT_TIMESTAMP)'
+        cursor.execute(query)
+        db.commit()
 
     def put(self):
         pass
